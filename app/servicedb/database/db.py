0f41a52c8e95485667df303c02d5ed5f617fa028
RuslanKap <kaprus88@gmail.com>
2024-06-06 17:16:07 +0300

    add app

---
+++ app/servicedb/database/db.py
@@ -0,0 +1,13 @@
+import os
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+engine = create_async_engine(DATABASE_URL)
+Base = declarative_base()
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
